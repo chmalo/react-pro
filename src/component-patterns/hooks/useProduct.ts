f58796105b76eb1dee05a152902c89d5b5ef556b
Chmalo <chmalo.f@gmail.com>
2022-12-09 10:49:51 -0400

    feat carrito de compra v2

--- src/component-patterns/hooks/useProduct.ts
+++ src/component-patterns/hooks/useProduct.ts
@@ -1,10 +1,16 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {useProductProps} from "../interfaces/interfaces";
 
 export const useProduct = ({product, onChange, value = 0}: useProductProps) => {
     const [counter, setCounter] = useState(value);
+    const isControlled = useRef(!!onChange);
 
     const increaseBy = (value: number) => {
+
+        if(isControlled.current){
+            return onChange!({count: value, product})
+        }
+
         const newValue = Math.max(counter + value, 0);
         setCounter(newValue)
 
